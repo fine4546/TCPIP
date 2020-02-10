ada5f005d63847dcd5e55e6ab5db5d4f3d2c4aac
fine4546 <56993666+fine4546@users.noreply.github.com>
2020-02-10 15:32:54 +0900

    Add files via upload

--- server_taskone.c
+++ server_taskone.c
@@ -12,15 +12,13 @@ int main(void){
     int sock, client_sock;
     struct sockaddr_in addr, client_addr;
     char buffer[1024];
-    char s1[10];
-    int len, addr_len, recv_len;
+    int len, addr_len, recv_len=1;
  
     if((sock = socket(AF_INET, SOCK_STREAM, 0))<0){
         perror("socket ");
         return 1;
     }
  
-    memset(&s1, 0x00, sizeof(s1));
     memset(&addr, 0x00, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -42,51 +40,41 @@ int main(void){
  
     while((client_sock = accept(sock, (struct sockaddr *)&client_addr, &addr_len)) > 0){
         printf("clinet ip : %s\n", inet_ntoa(client_addr.sin_addr));
+
+	while(recv_len > 0) {
         
-	while(1) {
-        if((recv_len = recv(client_sock, buffer, 1024, 0)) < 0){
-            perror("recv ");
-            close(client_sock);
-            return 1;
-            break;
-        }
+	        if((recv_len = recv(client_sock, buffer, 1024, 0)) < 0){
+        	    perror("recv ");
+	            return 1;
+	            break;
+	        }
+
+		if(strncmp(buffer, "2", 1) == 0)
+		{
+		   system("sh audio2_enc.sh");
+		}
+		else if(strncmp(buffer, "3", 1) == 0 )
+		{
+		   system("sh audio3_enc.sh");
+		}
+	        else if(strncmp(buffer, "q", 1) == 0)
+	        {
+	           break;
+	        }
 		
-	if( recv_len <= 0 ) {
-            close(client_sock);
-	    break;
-	}	    
+	        buffer[recv_len] = '\0';
+	        printf(". %s\n", buffer);
 
-        buffer[recv_len] = '\0';
-        printf(". %s\n", buffer);
-
-	if(strncmp(buffer, "2", 1) == 0)
-	{
-	   system("sh audio2_enc.sh");
 	}
-	else if(strncmp(buffer, "3", 1) == 0 )
-	{
-	   system("sh audio3_enc.sh");
-	}
-        else if(strncmp(buffer, "quit", 4) == 0)
-        {
-           break;
-        }
 
-	
- 	}
+	 
         close(client_sock);
-	printf("client leave \n");
-
-	scanf("%s", s1);
-	if(strncmp(s1, "end", 3) == 0)
-	{
-	   memset(&s1, 0x00, sizeof(s1));
-	   break;
-        }
+	printf("client leave\n");
+	recv_len = 1;
     }
 
-    printf("server end \n");
     close(sock);
     return 0;
 }
+
 
